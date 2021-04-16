688ea6b01266fecf14ff37e275778c28dca7693d
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-04-16 20:39:06 +0530

    now changes according to user status

--- client/myblog/src/Navbar.js
+++ client/myblog/src/Navbar.js
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import { UserContext } from "./UserContext";
 const Navbar = () => {
-	const token = localStorage.getItem("username");
+	const [state, setState] = useContext(UserContext);
+
+	useEffect(()=>{},[state])
+
 	const handleLogout = () => {
 		localStorage.removeItem("username");
 		localStorage.removeItem("usertoken");
 		localStorage.removeItem("userid");
+		setState(null);
 	};
 	return (
 		<>
-			{token !== null && (
+			{state !== null && (
 				<>
 					<Link to="/">
 						<h3>Home</h3>
