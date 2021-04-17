425bd8bdaa19481898c0491cc75d13488a7ca083
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-04-17 21:02:48 +0530

    now had "Profile" route

--- client/myblog/src/App.js
+++ client/myblog/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
-import Myblogs from "./pages/Myblogs";
 import Navbar from "./Navbar";
 import EachBLog from "./pages/EachBlog";
 import Edit from "./pages/Edit";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import { UserProvider } from "./UserContext";
+import Profile from "./pages/Profile";
 
 function App() {
 	return (
@@ -21,9 +21,6 @@ function App() {
 					<Route exact path="/create">
 						<Create />
 					</Route>
-					<Route exact path="/myblogs">
-						<Myblogs />
-					</Route>
 					<Route exact path="/Eachblog/:id">
 						<EachBLog />
 					</Route>
@@ -35,6 +32,9 @@ function App() {
 					</Route>
 					<Route exact path="/Signin">
 						<Signin />
+					</Route>
+					<Route exact path="/Profile/:author">
+						<Profile />
 					</Route>
 				</Switch>
 			</Router>
