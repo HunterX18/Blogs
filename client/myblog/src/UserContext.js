be40180d3e5de19acd8bacafb502f6b785acd7c9
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-04-17 21:04:01 +0530

    stores username as the state instead of usertoken

--- client/myblog/src/UserContext.js
+++ client/myblog/src/UserContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-	const [state, setState] = useState(localStorage.getItem("usertoken"));
+	const [state, setState] = useState(localStorage.getItem("username"));
 	return (
 		<UserContext.Provider value={[state, setState]}>
 			{children}
