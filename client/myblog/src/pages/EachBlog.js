f4f79e7fd60ece2f8cf3b0364bd538c990f00ef0
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-04-17 21:04:35 +0530

    can now direct to profile page of author

--- client/myblog/src/pages/EachBlog.js
+++ client/myblog/src/pages/EachBlog.js
@@ -7,7 +7,6 @@ const EachBlog = () => {
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
 	const [authorid, setAuthorid] = useState("");
-
 	const [loading, setLoading] = useState(false);
 	const userid = localStorage.getItem("userid");
 	const { id } = useParams();
@@ -16,7 +15,6 @@ const EachBlog = () => {
 		fetch(`/eachblog/${id}`)
 			.then((res) => res.json())
 			.then((result) => {
-				console.log(result);
 				setAuthor(result.author);
 				setTitle(result.title);
 				setBody(result.body);
@@ -25,7 +23,6 @@ const EachBlog = () => {
 			})
 			.catch((err) => console.log(err));
 	}, []);
-
 	return (
 		<>
 			<h1>This is blog page!</h1>
@@ -34,7 +31,9 @@ const EachBlog = () => {
 			) : (
 				<>
 					<h1>{title}</h1>
-					<h1>{author}</h1>
+					<Link to={`/Profile/${author}`}>
+						<h2>{author}</h2>
+					</Link>
 					<h1>{body}</h1>
 					{authorid == userid && (
 						<Link to={`/Edit/${id}`}>
